d884d21e768f7a8a9d9a0ea57771a2fff72c2fb0
Ankit Kumar <ankit@zpqv.com>
2025-03-27 22:23:54 +0530

    Changed port to 8080

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 8080);
+  const port = parseInt(process.env.PORT || '8080', 10); // Azure Linux expects 8080
+  await app.listen(port);
 }
 bootstrap();
